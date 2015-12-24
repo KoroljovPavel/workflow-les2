b0720e96a6e56174f7663d104873b8057604208f
Pavel Koroljov <afgmeister@gmail.com>
2015-12-24 23:32:08 +0200

    Start workflow-les2

---
+++ README.md
@@ -0,0 +1,13 @@
+##Это страница инернет магазина, она же задание от школы онлайн образования loftschool.
+###В этом проекте есть техники вёрстки javascript, sass, jade, BAM-naming.
+
+Чтобы скачать и посмотреть у себя на компьютере сайт нужно:
+
+В консоли GIT Bash ввести следующие команды:
+
+1.  git clone https://github.com/KoroljovPavel/workflow-les2.git "site" (site - папка в которую скопируется сайт)
+2.  cd "site" - (перейти в каталог с сайтом)
+3.  bower install - (установить bower)
+4.  npm install - (установить node.js)
+5.  gulp watch - (запустить gulp)
+=======
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var plumber = require('gulp-plumber');
+var compass = require('gulp-compass');
+var	browserSync = require('browser-sync');
+
+gulp.task('server', function() {
+	browserSync({
+		port: 9000,
+		server: {
+			baseDir: 'app/pages'
+		}
+	});
+});
+
+gulp.task('reload', function() {
+	gulp.watch([
+		'app/pages/*.html',
+		'app/js/**/*.js',
+		'app/css/**/*.css'
+	]).on('change', browserSync.reload);
+});
+var jadePath = 'app/markups/_pages/*.jade';
+gulp.task('jade', function() {
+	var YOUR_LOCALS = {};
+
+	gulp.src(jadePath)
+			.pipe(plumber())
+			.pipe(jade({
+				locals: YOUR_LOCALS,
+				pretty : '\t',
+			}))
+			.pipe(gulp.dest('app/pages'))
+});
+
+
+
+gulp.task('compass', function() {
+	gulp.src('app/sass/main.scss')
+			.pipe(plumber())
+			.pipe(compass({
+				config_file: 'app/config.rb',
+				css: 'app/css',
+				sass: 'app/sass'
+			}))
+});
+
+
+gulp.task('watch', function(){
+	gulp.run('server');
+	gulp.run('reload');
+	gulp.watch(jadePath, ['jade']);
+	gulp.watch('app/sass/**/*.scss', ['compass'])
+});
